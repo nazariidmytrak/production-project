267a3830796087d78cc66018e61a3e9b287163e7
DmytrakNazariy <dmutraknazariy@gmail.com>
2023-11-14 11:32:02 +0200

    Small fixes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,12 +6,11 @@ import { Navbar } from 'widgets/Navbar';
 import { classNames } from 'shared/lib/classNames/classNames';
 
 const App = () => {
-  const { theme, toggleTheme } = useTheme();
+  const { theme } = useTheme();
   return (
     <div className={classNames('app', {}, [theme])}>
       <Navbar />
       <AppRouter />
-      <button onClick={toggleTheme}>Toggle</button>
     </div>
   );
 };
