35534138b78ff5ee1ca4d3b511c77b1325b04120
DmytrakNazariy <dmutraknazariy@gmail.com>
2023-11-14 10:23:23 +0200

    Navbar, AppLink

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,17 @@
-import { Link } from 'react-router-dom';
-
 import './styles/index.scss';
 
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router';
+import { Navbar } from 'widgets/Navbar';
 import { classNames } from 'shared/lib/classNames/classNames';
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
   return (
     <div className={classNames('app', {}, [theme])}>
-      <button onClick={toggleTheme}>Toggle</button>
-      <Link to={'/'}>Main page</Link>
-      <Link to={'/about'}>About</Link>
+      <Navbar />
       <AppRouter />
+      <button onClick={toggleTheme}>Toggle</button>
     </div>
   );
 };
--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,7 +1,7 @@
 import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import { routeConfig } from 'shared/config/routeConfig/RouteConfig';
+import { routeConfig } from 'shared/config/routeConfig/routeConfig';
 
 const AppRouter = () => {
   return (
