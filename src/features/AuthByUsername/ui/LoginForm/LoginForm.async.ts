79dcd1e1db83b9252655a39fb440036af9343e9c
DmytrakNazariy <dmutraknazariy@gmail.com>
2024-01-03 10:12:42 +0200

    production

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.async.ts
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.async.ts
@@ -3,7 +3,5 @@ import { FC, lazy } from 'react';
 import { LoginFormProps } from './LoginForm';
 
 export const LoginFormAsync = lazy<FC<LoginFormProps>>(
-  () => new Promise((resolve) => {
-    setTimeout(() => resolve(import('./LoginForm')), 300);
-  })
+  () => import('./LoginForm')
 );
