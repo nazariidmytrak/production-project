1de99a28b0038252b8bf37ef74addda57d067b9d
DmytrakNazariy <dmutraknazariy@gmail.com>
2024-01-31 14:08:41 +0200

    storybook alias fix

--- src/features/profileRating/ui/ProfileRating/ProfileRating.stories.tsx
+++ src/features/profileRating/ui/ProfileRating/ProfileRating.stories.tsx
@@ -1,18 +1,18 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { ProfileRating } from './ProfileRating';
+import ProfileRating from './ProfileRating';
 
 export default {
   title: 'features/ProfileRating',
   component: ProfileRating,
   argTypes: {
-  backgroundColor: { control: 'color' },
+    backgroundColor: { control: 'color' },
   },
 } as ComponentMeta<typeof ProfileRating>;
 
-const Template: ComponentStory<typeof ProfileRating> = (args) => <ProfileRating {...args} />;
+const Template: ComponentStory<typeof ProfileRating> = (args) => (
+  <ProfileRating {...args} />
+);
 
 export const Primary = Template.bind({});
-Primary.args = {
-   
-};
+Primary.args = {};
