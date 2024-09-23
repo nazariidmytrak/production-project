9dc164d95615d6dc75e86d3039aa7cb9f86e2e70
DmytrakNazariy <dmutraknazariy@gmail.com>
2024-09-23 11:01:06 +0300

    webpack config upgrade

--- config/build/loaders/buildBabelLoader.ts
+++ config/build/loaders/buildBabelLoader.ts
@@ -6,6 +6,7 @@ interface BuildBabelLoaderProps extends BuildOptions {
 }
 
 export function buildBabelLoader({ isDev, isTsx }: BuildBabelLoaderProps) {
+  const isProd = !isDev;
   return {
     test: isTsx ? /\.(jsx|tsx)$/ : /\.(js|ts)$/,
     exclude: /node_modules/,
@@ -21,7 +22,7 @@ export function buildBabelLoader({ isDev, isTsx }: BuildBabelLoaderProps) {
             },
           ],
           '@babel/plugin-transform-runtime',
-          isTsx && [babelRemovePropsPlugin, { props: ['data-testid'] }],
+          isTsx && isProd && [babelRemovePropsPlugin, { props: ['data-testid'] }],
           isDev && require.resolve('react-refresh/babel'),
         ].filter(Boolean),
       },
