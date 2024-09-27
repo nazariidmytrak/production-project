b87408c0d40876409a628cc3a3eeaa0e7dbd91b9
DmytrakNazariy <dmutraknazariy@gmail.com>
2024-09-27 13:56:39 +0300

    added argument for buildSelector

--- src/pages/ArticlesPage/model/selectors/articlesPageSelectors.ts
+++ src/pages/ArticlesPage/model/selectors/articlesPageSelectors.ts
@@ -1,13 +1,11 @@
 import { StateSchema } from '@/app/providers/StoreProvider';
 import { ArticleView, ArticleSortField, ArticleType } from '@/entities/Article';
+import { buildSelector } from '@/shared/lib/store';
 
-// eslint-disable-next-line
 export const getArticlesPageIsLoading = (state: StateSchema) =>
   state.articlesPage?.isLoading || false;
-
 export const getArticlesPageError = (state: StateSchema) =>
   state.articlesPage?.error;
-
 export const getArticlesPageInited = (state: StateSchema) =>
   state.articlesPage?._inited;
 
@@ -20,16 +18,17 @@ export const getArticlesPageHasMore = (state: StateSchema) =>
   state.articlesPage?.hasMore;
 
 // filters
-// eslint-disable-next-line
 export const getArticlesPageView = (state: StateSchema) =>
   state.articlesPage?.view || ArticleView.SMALL;
 export const getArticlesPageOrder = (state: StateSchema) =>
   state.articlesPage?.order ?? 'asc';
-// eslint-disable-next-line
 export const getArticlesPageSort = (state: StateSchema) =>
   state.articlesPage?.sort ?? ArticleSortField.CREATED;
 export const getArticlesPageSearch = (state: StateSchema) =>
   state.articlesPage?.search ?? '';
-// eslint-disable-next-line
 export const getArticlesPageType = (state: StateSchema) =>
   state.articlesPage?.type ?? ArticleType.ALL;
+
+export const [useArticleItemById] = buildSelector(
+  (state, id: string) => state.articlesPage?.entities[id],
+);
