2ced58c58753d29c917f1933f49ea42fa16aacf1
DmytrakNazariy <dmutraknazariy@gmail.com>
2024-11-25 10:08:18 +0200

    feat(features/Notification): add new stories and update for Storybook compatibility

--- src/features/notificationButton/ui/NotificationButton/NotificationButton.stories.tsx
+++ src/features/notificationButton/ui/NotificationButton/NotificationButton.stories.tsx
@@ -1,20 +1,82 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
 
+import { ThemeDecorator } from '@/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from '@/shared/const/theme';
 import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
 import { NotificationButton } from './NotificationButton';
+import { NewDesignDecorator } from '@/shared/config/storybook/NewDesignDecorator/NewDesignDecorator';
 
-export default {
+const meta: Meta<typeof NotificationButton> = {
   title: 'features/Notification/NotificationButton',
   component: NotificationButton,
-  argTypes: {
-    backgroundColor: { control: 'color' },
+  decorators: [StoreDecorator({})],
+};
+
+export default meta;
+type Story = StoryObj<typeof NotificationButton>;
+
+export const Primary: Story = {
+  decorators: [NewDesignDecorator],
+  parameters: {
+    docs: {
+      description: {
+        story: 'Shows the primary state of the NotificationButton component.',
+      },
+    },
   },
-} as ComponentMeta<typeof NotificationButton>;
+};
 
-const Template: ComponentStory<typeof NotificationButton> = (args) => (
-  <NotificationButton {...args} />
-);
+export const Dark: Story = {
+  decorators: [NewDesignDecorator, ThemeDecorator(Theme.DARK)],
+  parameters: {
+    docs: {
+      description: {
+        story: 'Shows the NotificationButton in dark theme.',
+      },
+    },
+  },
+};
 
-export const Primary = Template.bind({});
-Primary.args = {};
-Primary.decorators = [StoreDecorator({})];
+export const Orange: Story = {
+  decorators: [NewDesignDecorator, ThemeDecorator(Theme.ORANGE)],
+  parameters: {
+    docs: {
+      description: {
+        story: 'Shows the NotificationButton in orange theme.',
+      },
+    },
+  },
+};
+
+export const PrimaryDeprecated: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'Shows the deprecated state of the NotificationButton component.',
+      },
+    },
+  },
+};
+
+export const DarkDeprecated: Story = {
+  decorators: [ThemeDecorator(Theme.DARK)],
+  parameters: {
+    docs: {
+      description: {
+        story: 'Shows the deprecated NotificationButton in dark theme.',
+      },
+    },
+  },
+};
+
+export const OrangeDeprecated: Story = {
+  decorators: [ThemeDecorator(Theme.ORANGE)],
+  parameters: {
+    docs: {
+      description: {
+        story: 'Shows the deprecated NotificationButton in orange theme.',
+      },
+    },
+  },
+};
